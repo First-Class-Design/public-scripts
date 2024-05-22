802757caeffeba88adff1c66f61f6e7109b135f2
matt-fcd <matt@fcd.org.uk>
2024-05-22 16:39:37 +0100

    Create copy-url.js

---
+++ scripts/copy-url.js
@@ -0,0 +1,15 @@
+/* Copy URL */
+const linkCopy = document.getElementById('copy-url');
+linkCopy.addEventListener("click", copyURL);
+  function copyURL() {
+    let url = document.location.href;
+    const popup = document.getElementById('site_notice_success');
+    const error = document.getElementById('site_notice_error');
+    navigator.clipboard.writeText(url).then(function() {
+      popup.classList.add('show');
+      setTimeout(() => {popup.classList.remove('show');}, 1200);
+    }, function() {
+      error.classList.add('show');
+      setTimeout(() => {error.classList.remove('show');}, 1200);
+    });
+  }
