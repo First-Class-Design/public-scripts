2b578d96357f75ad5da7a01cf2154f407f697b08
ReeceFCD <reece@fcd.org.uk>
2025-08-28 10:06:41 +0100

    Update fcd-post-date-ago.js
    - Updated to JS ES6 from jQuery.

--- scripts/fcd-post-date-ago.js
+++ scripts/fcd-post-date-ago.js
@@ -1,64 +1,42 @@
-function timePast(curr, prev) {
-    //define the milliseconds in every time unit
-    var msMin = 60 * 1000;
-    var msHr = msMin * 60;
-    var msDay = msHr * 24;
-    var msWeek = msDay * 7;  
-    var msMonth = msDay * 30;
-    var msYr = msDay * 365;
-    //get elapsed time in milliseconds
-    var elapsed = curr - prev;
+  // Function to pluralize the time past (eg. minute or minutes / day or days)
+  const pluralize = (count, noun, suffix = 's') => `${count} ${noun}${count !== 1 ? suffix : ''}`;
 
-    if (elapsed < msMin) {
-        return Math.round(elapsed / 1000) + ' seconds ago';
-    } else if (elapsed < msHr) {
-        var elapsed = Math.round(elapsed / msMin);
-        if (elapsed === 1) {            
-            return elapsed + ' minute ago';
-        } else {
-            return elapsed + ' minutes ago';
-        }
-    } else if (elapsed < msDay) {
-        var elapsed = Math.round(elapsed / msHr);
-        if (elapsed === 1) {
-            return elapsed + ' hour ago';
-        } else {
-            return elapsed + ' hours ago';
-        }
-    } else if (elapsed < msWeek) {
-        var elapsed = Math.round(elapsed / msDay);
-        if (elapsed === 1) {
-            return elapsed + ' day ago';
-        } else {
-            return elapsed + ' days ago';
-        }
-    } else if (elapsed < msMonth) {
-        var elapsed = Math.round(elapsed / msWeek);
-        if (elapsed === 1) { // Check for week
-            return elapsed + ' week ago';
-        } else {
-            return elapsed + ' weeks ago';
-        }
-    } else if (elapsed < msYr) {
-        var elapsed = Math.round(elapsed / msMonth);
-        if (elapsed === 1) {
-            return elapsed + ' month ago';
-        } else {
-            return elapsed + ' months ago';
-        }
-    } else {
-        var elapsed = Math.round(elapsed / msYr);
-        if (elapsed === 1) {
-            return elapsed + ' year ago';
-        } else {
-            return elapsed + ' years ago';
-        }
+  const timePast = (curr, prev) => {
+    const msMin = 60 * 1000, msHr = msMin * 60, msDay = msHr * 24, msWeek = msDay * 7, msMonth = msDay * 30, msYr = msDay * 365;
+    let elapsed = curr - prev;
+
+    if (elapsed < msMin) return pluralize(Math.round(elapsed/1000), 'second');
+    else if (elapsed < msHr) {
+      elapsed = Math.round(elapsed/msMin);
+      return pluralize(elapsed, 'minute');
     }
-}
-$(document).ready(function () {
-    $('.u-post-date').each(function () {
-        var now = new Date();
-        var parsedTime = Date.parse($(this).text());
-        $(this).text(timePast(now, new Date(parsedTime)));
-    });
-});
+    else if (elapsed < msDay) {
+      elapsed = Math.round(elapsed/msHr);
+      return pluralize(elapsed, 'hour');
+    }
+    else if (elapsed < msWeek) {
+        elapsed = Math.round(elapsed/msDay);
+        return pluralize(elapsed, 'week');
+    }
+    else if (elapsed < msMonth) {
+      elapsed = Math.round(elapsed/msDay);
+      return pluralize(elapsed, 'day'); 
+    }
+    else if (elapsed < msYr) {
+      elapsed = Math.round(elapsed/msMonth);
+      return pluralize(elapsed, 'month');
+    }
+    else {
+      elapsed = Math.round(elapsed/msYr);
+      return pluralize(elapsed, 'year');
+    }
+  }
+
+  let now = new Date();
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.u-post-date-ago').forEach(box => {
+        let parsedTime = Date.parse(box.innerText);
+        box.innerText = timePast(now, new Date(parsedTime)) + ' ago';
+    })
+})
